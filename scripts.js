f35a7ed1d7323bf2ece65e97186dab84af9d5fb8
Marcos Cejas <marcoscejas@outlook.com>
2023-02-26 18:34:43 -0300

    agregado animaciones y estilizado general

--- scripts.js
+++ scripts.js
@@ -5,8 +5,18 @@ $(document).ready(function() {
         var $content = $(target);
         var originalText = $this.data("text");    
 
+        // Close other content sections
+        $(".show-more").not($this).each(function() {
+            var $otherContent = $($(this).data("target"));
+            var otherOriginalText = $(this).data("text");
+            if ($otherContent.hasClass("showContent")) {
+                $otherContent.toggleClass("showContent hideContent");
+                $(this).text(otherOriginalText);
+            }
+        });
+
         if($this.text() === originalText){
-            $this.text("Mostrar menos");
+            $this.text(originalText);
             $content.toggleClass("hideContent showContent");
         } else {
             $this.text(originalText);
@@ -14,3 +24,17 @@ $(document).ready(function() {
         };
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.animate-button');
+    buttons.forEach(button => {
+      button.addEventListener('mouseenter', () => {
+        console.log('mouseenter event triggered');
+        button.classList.add('animate__animated', 'animate__pulse');
+      });
+      button.addEventListener('mouseleave', () => {
+        console.log('mouseleave event triggered');
+        button.classList.remove('animate__animated', 'animate__pulse');
+      });
+    });
+  });
