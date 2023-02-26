1f33668a2a824151fe00c541479b6f0fc1777d92
dummymarcos <123195582+dummymarcos@users.noreply.github.com>
2023-02-26 18:55:09 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,19 +1,40 @@
-$(document).ready(function() {  
-    
-    $(".show-more button").on("click", function() {
+$(document).ready(function() {
+    $(".show-more").on("click", function() {
         var $this = $(this); 
-        var $content = $("#hidden");
-        var linkText = $this.text();    
+        var target = $this.data("target");
+        var $content = $(target);
+        var originalText = $this.data("text");    
 
-        
-        if(linkText === "Show more"){
-            linkText = "Show less";
+        // Close other content sections
+        $(".show-more").not($this).each(function() {
+            var $otherContent = $($(this).data("target"));
+            var otherOriginalText = $(this).data("text");
+            if ($otherContent.hasClass("showContent")) {
+                $otherContent.toggleClass("showContent hideContent");
+                $(this).text(otherOriginalText);
+            }
+        });
+
+        if($this.text() === originalText){
+            $this.text(originalText);
             $content.toggleClass("hideContent showContent");
         } else {
-            linkText = "Show more";
+            $this.text(originalText);
             $content.toggleClass("showContent hideContent");
         };
-    
-        $this.text(linkText);
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.animate-button');
+    buttons.forEach(button => {
+      button.addEventListener('mouseenter', () => {
+        console.log('mouseenter event triggered');
+        button.classList.add('animate__animated', 'animate__pulse');
+      });
+      button.addEventListener('mouseleave', () => {
+        console.log('mouseleave event triggered');
+        button.classList.remove('animate__animated', 'animate__pulse');
+      });
+    });
+  });
